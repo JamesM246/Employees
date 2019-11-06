7487bd0418bdd822573860468702b1a182fffb42
James Maguire <james.maguire246@hotmail.co.uk>
2019-11-06 10:35:32 +0000

    Upload to database using threading

--- src/main/java/com/sparta/jmm/manager/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.jmm.manager;
-
-public class Main {
-    public static void main(String[] args){
-        EmployeeManager employeeManager = new EmployeeManager();
-//        employeeManager.addFromFileToDB("resources/EmployeeRecordsTestData.csv");
-        employeeManager.addFromFileToDB("resources/EmployeeRecords.csv");
-    }
-}
